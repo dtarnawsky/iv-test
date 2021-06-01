c067343aabe9651b87c7b101bad694686a87fc50
Damian Tarnawsky <damian@ionic.io>
2021-06-01 07:10:33 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'iv-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
