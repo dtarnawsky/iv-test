14d6e1eecda0fa0c9b316deeeed78c4a25280eb3
Damian Tarnawsky <damian@ionic.io>
2021-10-01 09:26:26 -0700

    Add PKCE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -30,7 +30,8 @@ export const nativeIonicAuthOptions: IonicAuthOptions = {
   // to share site data with the app. 'private' uses a webview which will
   // not prompt the user but will not be able to share session/cookie data
   // either for true SSO across multiple apps.
-  iosWebView: 'private'  
+  iosWebView: 'private',
+  webAuthFlow: 'PKCE'  
 };
 
 export const webIonicAuthOptions: IonicAuthOptions = {
