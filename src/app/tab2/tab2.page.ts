40b250a5269faaf5000042f722c3bbd7f667c91d
Damian Tarnawsky <damian@ionic.io>
2022-11-16 08:18:23 -0800

    Add test scenario on tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device, DeviceSecurityType, IdentityVaultConfig, Vault, VaultError, VaultType } from '@ionic-enterprise/identity-vault';
+import { VaultLockEvent } from '@ionic-enterprise/identity-vault/dist/typings/VaultInterface';
+import { VaultService } from '../vault.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,48 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  config: IdentityVaultConfig = {
+    key: 'io.ionic.iv-test-both',
+    type: VaultType.DeviceSecurity,
+    deviceSecurityType: DeviceSecurityType.Both,
+    unlockVaultOnLoad: false,
+  };
+
+  vault: Vault;
+
+  constructor() { }
+
+  async getStatus() {
+    alert(`isSystemPasscodeSet=${await Device.isSystemPasscodeSet()} isBiometricsEnabled=${await Device.isBiometricsEnabled()}`);
+  }
+
+  setVault() {
+    this.vault = new Vault(this.config);
+    this.vault.onError((err: VaultError) => {
+      alert('Vault Error: ' + JSON.stringify(err));
+    });
+    this.vault.onLock((lockEvent: VaultLockEvent) => {
+      alert('Vault is Locked ' + JSON.stringify(lockEvent));
+    });
+    this.vault.onUnlock(() => {
+      alert('Vault is unlocked');
+    });
+  }
+
+  async setVaultValue() {
+    try {
+      await this.vault.setValue('stuff', 'things');
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  async clearVault() {
+    try {
+      await this.vault.clear();
+    } catch (err) {
+      alert(err);
+    }
+  }
 
 }
